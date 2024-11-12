a45bdbe08310f24c3a34dc1863bd9929758236d7
Goitom <goitommahtsentu@gmail.com>
2024-11-12 21:20:08 +0100

    frontend adding headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Header from "./components/Header.jsx";
 
 const App = () => {
     return (
-        <div className='text-red-500'>
-            app
-        </div>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/profile' element={<Profile/>}/>
+                <Route path='/sign-in' element={<SignIn/>}/>
+                <Route path='/sign-up' element={<SignUp/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
