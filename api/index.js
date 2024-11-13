d74c58ddc998c4b59524d87d948d1ddfbfe0d5a1
Goitom <goitommahtsentu@gmail.com>
2024-11-13 01:32:12 +0100

    first commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
 const app= express();
+dotenv.config()
 
-
+mongoose.connect(process.env.MONG_URL).then(()=>{
+   console.log('connected to Db')
+}).catch((err)=>{
+   console.log(err)
+})
 app.listen(3000,(req,res)=>{
    console.log('test')
 })
