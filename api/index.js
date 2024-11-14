b74639926e50a1fe6ec630a0f6cfe6663c4573f9
Goitom <goitommahtsentu@gmail.com>
2024-11-14 17:24:41 +0100

    signin and signup complete

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from "mongoose";
+// import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 const app= express();
 dotenv.config()
-
+app.use(express.json())
 mongoose.connect(process.env.MONG_URL).then(()=>{
    console.log('connected to Db')
 }).catch((err)=>{
@@ -12,3 +14,17 @@ mongoose.connect(process.env.MONG_URL).then(()=>{
 app.listen(3000,(req,res)=>{
    console.log('test')
 })
+// app.use('/api/user',userRouter)
+app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+
+
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
